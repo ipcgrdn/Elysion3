9a932e96e266b1d3228e7d22fbcf784c8c74bdd0
choi <cejewe@naver.com>
2024-09-11 15:18:43 +0900

    fix

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+
+import { SignInFlow } from "../types";
+import { useAuthActions } from "@convex-dev/auth/react";
+
+import { FcGoogle } from "react-icons/fc";
+import { SiNaver } from "react-icons/si";
+import { TriangleAlert } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+interface SignInCardProps {
+    setState: (state: SignInFlow) => void;
+};
+
+export const SignInCard = ({
+    setState
+}: SignInCardProps) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [pending, setPending] = useState(false);
+
+    const {signIn} =  useAuthActions();
+
+    const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        setPending(true);
+        signIn("password", { email, password, flow: "signIn"})
+            .catch(() => {
+                setError("Invalid email or password")
+            })
+            .finally(() => {
+                setPending(false);
+            });
+    };
+
+    const onProviderSingIn = (value: "google" | "naver") => {
+        setPending(true);
+        signIn(value)
+            .finally(() => {
+                setPending(false);
+            });
+    };
+
+    return (
+        <Card className="w-full h-full p-8">
+            <CardHeader className="px-0 pt-0">
+                <CardTitle>
+                    Login to continue
+                </CardTitle>
+                <CardDescription>
+                    Use your email or another service to continue
+                </CardDescription>
+            </CardHeader>
+            {!!error && (
+                <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+                    <TriangleAlert className="size-4" />
+                    <p>{error}</p>
+                </div>
+            )}
+            <CardContent className="space-y-5 px-0 pb-0">
+                <form 
+                    onSubmit={onPasswordSignIn}
+                    className="space-y-2.5"
+                >
+                    <Input
+                        disabled={pending}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                        type="email"
+                        required
+                    />
+                    <Input
+                        disabled={pending}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                        type="password"
+                        required
+                    />
+                    <Button
+                        type="submit"
+                        className="w-full"
+                        size="lg"
+                        disabled={pending}
+                    >
+                        Continue
+                    </Button>
+                </form>
+                <Separator />
+                <div className="flex flex-col gap-y-2.5">
+                    <Button
+                        disabled={pending}
+                        onClick={() => onProviderSingIn("google")}
+                        variant="outline"
+                        size="lg"
+                        className="w-full relative"
+                    >
+                        <FcGoogle className="size-5 absolute top-3 left-2.5" />
+                        Continue with Google
+                    </Button>
+                    <Button
+                        disabled={pending}
+                        onClick={() => onProviderSingIn("naver")}
+                        variant="outline"
+                        size="lg"
+                        className="w-full relative"
+                    >
+                        <SiNaver className="size-4 absolute top-3 left-3 text-[#2DB400]" />
+                        Continue with Naver
+                    </Button>
+                </div>
+                <p className="text-xs text-muted-foreground">
+                    Don&apos;t have an account? <span onClick={() => setState("signUp")} className="text-sky-700 hover:underline cursor-pointer">Sign up</span>
+                </p>
+            </CardContent>
+        </Card>
+    );
+};
---
+++ src/features/members/api/use-current-member.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "convex/react";
+
+import { Id } from "../../../../convex/_generated/dataModel"
+import { api } from "../../../../convex/_generated/api";
+
+interface useCurrentMemberProps {
+    workspaceId: Id<"workspaces">;
+}
+
+export const useCurrentMember = ({
+    workspaceId
+}: useCurrentMemberProps) => {
+    const data = useQuery(api.members.current, { workspaceId });
+    const isLoading = data === undefined;
+
+    return { data, isLoading };
+};
